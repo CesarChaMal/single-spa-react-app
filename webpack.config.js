d5ed66383f7aa2c50c3d124e5a36420ca65e365a
Cesar Chavez <cesarchanalu@gmail.com>
2025-08-12 03:46:28 +0200

    Update.

--- webpack.config.js
+++ webpack.config.js
@@ -3,28 +3,48 @@ const webpack = require('webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: ['src/singleSpaEntry.js'],
+  entry: ['./src/singleSpaEntry.js'],
   output: {
     library: 'single-spa-react-app',
     libraryTarget: 'umd',
     filename: 'single-spa-react-app.js',
     path: path.resolve(__dirname, 'dist'),
-	publicPath: 'http://localhost:4206/'
+    publicPath: 'http://localhost:4206/',
   },
   module: {
     rules: [
+      // JS (drop eslint-loader to avoid the flowtype plugin warnings)
       {
-        test: /\.js?$/,
-        exclude: [path.resolve(__dirname, 'node_modules')],
-        loader: ['babel-loader', 'eslint-loader'],
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
       },
+
+      // HTML
       {
         test: /\.html$/i,
-        loader: 'html-loader',
+        use: ['html-loader'],
       },
+
+      // Plain CSS -> NO sass-loader here
       {
-        test: /\.css|\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+
+      // SCSS/SASS -> use sass-loader with Dart Sass
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+            },
+          },
+        ],
       },
     ],
   },
@@ -33,23 +53,18 @@ module.exports = {
   },
   resolve: {
     modules: [__dirname, 'node_modules'],
+    extensions: ['.js', '.json'],
   },
   plugins: [
-    new CleanWebpackPlugin({
-      cleanAfterEveryBuildPatterns: ['dist'],
-    }),
-    new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1,
-    }),
+    new CleanWebpackPlugin({ cleanAfterEveryBuildPatterns: ['dist'] }),
+    new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
   ],
   devtool: 'source-map',
   externals: [],
   devServer: {
-	port: 4206,
+    port: 4206,
     historyApiFallback: true,
     writeToDisk: true,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
+    headers: { 'Access-Control-Allow-Origin': '*' },
   },
 };
